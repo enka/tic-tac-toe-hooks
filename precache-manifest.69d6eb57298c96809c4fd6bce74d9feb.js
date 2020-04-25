397b7f8d1ca506d765d5a55dc95aa176e1595cef
Antonio Cuenca <1935915+enka@users.noreply.github.com>
2020-04-25 19:51:11 +0200

    Updates

---
+++ precache-manifest.69d6eb57298c96809c4fd6bce74d9feb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6da3cc2efd563a9f047649a66861fcf",
+    "url": "/tic-tac-toe-hooks/index.html"
+  },
+  {
+    "revision": "19369e2a2ccec97db982",
+    "url": "/tic-tac-toe-hooks/static/css/main.bb61f647.chunk.css"
+  },
+  {
+    "revision": "fe42fc499c07ec498de2",
+    "url": "/tic-tac-toe-hooks/static/js/2.19552c45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-hooks/static/js/2.19552c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19369e2a2ccec97db982",
+    "url": "/tic-tac-toe-hooks/static/js/main.748aa29e.chunk.js"
+  },
+  {
+    "revision": "689ee9b41f9dd4fad790",
+    "url": "/tic-tac-toe-hooks/static/js/runtime-main.f8a2a5cf.js"
+  }
+]);
